34dc07c25af432a3d961e463d71343c1284ec68d
Marina Gladysheva <marina@lifter.ai>
2024-08-22 18:49:30 +0200

    fix text

--- custom_auth_test.go
+++ custom_auth_test.go
@@ -17,7 +17,6 @@ func TestCustomAuth(t *testing.T) {
 			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
-		json.NewEncoder(w).Encode(map[string]interface{}{"id": "user123"})
 		err := json.NewEncoder(w).Encode(map[string]interface{}{"id": "user123"})
 		if err != nil {
 			t.Fatalf("Failed to encode response: %v", err)
